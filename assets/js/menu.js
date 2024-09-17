3bcf77a0cca5850f0c972d48f3c483d11bd141b8
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2024-09-17 09:00:06 +0200

    update menu add logo in mobile view

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,12 +3,16 @@ console.log("hello menu.js");
 const navbarMenu = document.querySelector(".navbar-menu");
 const burger = document.querySelector(".burger");
 const closeBtn = document.querySelector(".close");
+const logoMobile = document.querySelector(".logoMobile");
+
 
 const showHideNavbar = () => {
   navbarMenu.classList.toggle("hideNav");
   navbarMenu.classList.toggle("showNav");
   closeBtn.classList.toggle("dnone");
   burger.classList.toggle("dnone");
+  logoMobile.classList.toggle("dnone");
+
 };
 burger.addEventListener("click", showHideNavbar);
 closeBtn.addEventListener("click", showHideNavbar);
