ec1ac5fe7eb963e10d53a992f158f2f6fd975a26
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2024-09-19 09:41:33 +0200

    menu mise en forme sous-menu user

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,5 +1,8 @@
 console.log("hello menu.js");
 
+// Format mobile 
+// affichage menu
+
 const navbarMenu = document.querySelector(".navbar-menu");
 const burger = document.querySelector(".burger");
 const closeBtn = document.querySelector(".close");
@@ -17,3 +20,35 @@ const showHideNavbar = () => {
 burger.addEventListener("click", showHideNavbar);
 closeBtn.addEventListener("click", showHideNavbar);
 
+
+// Tous affichages 
+// Menu déroulant pour compte user
+
+function subMenu(menuButton, menuBloc) {
+  const button = document.querySelector(menuButton);
+  const bloc = document.querySelector(menuBloc);
+  const menuFrame = button.nextElementSibling;
+  menuFrame.style.transition = "all 0s";
+  menuFrame.style.maxHeight = "0";
+  menuFrame.style.overflow = "hidden";
+  menuFrame.classList.toggle("navbar-menu-items-li");
+
+  bloc.addEventListener("mouseenter", () => {
+      menuFrame.classList.toggle("navbar-menu-items-li");
+      
+      menuFrame.style.maxHeight = "40rem";
+      menuFrame.style.overflow = "visible";
+      button.classList.toggle("category-active");
+  });
+  bloc.addEventListener("mouseleave", () => {
+     
+      menuFrame.classList.toggle("menu-category-items");
+      menuFrame.style.maxHeight = "0";
+      menuFrame.style.overflow = "hidden";
+      button.classList.toggle("category-active");
+  });
+}
+
+// j'appelle ma fonction
+
+subMenu("#userMenu", "#userMenuBloc");
